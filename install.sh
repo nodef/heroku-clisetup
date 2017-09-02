cfceb2efb3aca3a5966d2de4b2b1e5f7aa20dc53
Subhajit Sahu <wolfram77@gmail.com>
2017-09-02 21:05:52 +0530

    :two_women_holding_hands: goto main module path

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 id=${PWD##*/}
 url="$HEROKU_CLI_URL"
-lgn="$HEROKU_CLI_LOGIN"
-pwd="$HEROKU_CLI_PASSWORD"
-ach=$(cat arch.txt)
-pfm=$(cat platform.txt)
-if [[ $PWD == tmp/* ]]; then r=$PWD; else r=~; fi
+logn="$HEROKU_CLI_LOGIN"
+pass="$HEROKU_CLI_PASSWORD"
+arch=$(cat arch.txt)
+ptfm=$(cat platform.txt)
 uro="https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli"
-if [[ "$url" == "" ]]; then url="${uro}-${pfm}-${ach}.tar.gz"; fi
+if [[ "$url" == "" ]]; then url="${uro}-${ptfm}-${arch}.tar.gz"; fi
+while [[ $PWD == *node_modules* ]]; do cd ..; done
 echo "${id}: get working directory ..."
 echo "${PWD}"
+if [[ $PWD == /tmp* ]]; then r=$PWD; else r=~; fi
 echo "${id}: selecting home directory ..."
 echo "${r}"
 
@@ -18,7 +19,7 @@ if [ ! -e $r/.heroku-cli ]; then
   mkdir tmp-hero && cd tmp-hero
   echo "${id}: downloading heroku cli ..."
   echo "${url}"
-  if [[ "$pfm" == "windows" ]]; then
+  if [[ "$ptfm" == "windows" ]]; then
     powershell -command "& { iwr ${url} -OutFile heroku.tar.gz }" >/dev/null
   else
     wget -nv -O heroku.tar.gz "${url}" >/dev/null
@@ -35,11 +36,11 @@ if [ ! -e $r/.netrc ] && [ ! -e $r/_netrc ]; then
   echo "${id}: setting login information ..."
   echo "" >netrc
   echo "machine api.heroku.com" >>netrc
-  echo "  password ${pwd}" >>netrc
-  echo "  login ${lgn}" >>netrc
+  echo "  password ${pass}" >>netrc
+  echo "  login ${logn}" >>netrc
   echo "machine git.heroku.com" >>netrc
-  echo "  password ${pwd}" >>netrc
-  echo "  login ${lgn}" >>netrc
+  echo "  password ${pass}" >>netrc
+  echo "  login ${logn}" >>netrc
   cp netrc $r/.netrc
   mv netrc $r/_netrc
 fi
